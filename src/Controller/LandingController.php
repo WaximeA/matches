a37098cd044f8b60ad4465ddbc02f0108bf268c3
waxime <aveline.maxime@gmail.com>
2019-03-20 00:27:54 +0100

    global: Manage controllers and views with Bulma

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -5,13 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends AbstractController
+class LandingController extends AbstractController
 {
     /**
-     * @Route("/", name="index")
+     * @Route("/", name="landing_index")
      */
     public function index()
     {
-        return $this->render('pages/index.html.twig');
+        return $this->render('landing/index.html.twig');
     }
 }
