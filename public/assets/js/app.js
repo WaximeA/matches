8a96a56f2ec126f9b5f93b9ebcc261f164fc7423
waxime <aveline.maxime@gmail.com>
2019-03-21 00:04:05 +0100

    match-images: Manage celebrity image and match button apparition delay

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,39 +1,46 @@
 let input = $('.celebrity-input');
+let inputData = input.data('options');
 let matchLeftInput = $('#match-left').find('input');
 let matchRightInput = $('#match-right').find('input');
-let flames = $('.flames');
 let matchButton = $('#match-button');
+let flames = $('.flames');
+let timer = null;
 
-console.log(matchButton);
-
-// When edit celebrity input
-input.change(function(e) {
-  let query = $(this).val();
-  let parentDiv = $(this).parent();
+function addCelebrityImage(inputThis) {
+  let query = inputThis.val();
+  let parentDiv = inputThis.parent();
   let parentClass = parentDiv.attr('id');
   let currentMatchImage = parentDiv.find('.match-img');
-
-  console.log(query);
 
   if (query) {
     fetch('/api/getImages/' + query).
         then(response => response.json()).
         then(data => {
+          console.log(inputThis.data( "options" ));
           currentMatchImage.attr('src', data.data);
         }).
         catch(error => console.error(error));
   } else {
     currentMatchImage.attr('src', 'https://bulma.io/images/placeholders/128x128.png');
   }
+}
 
-});
-
-// Add match button
-input.on('input', function() {
+function displayMatchButton() {
   if (matchLeftInput.val() !== '' && matchRightInput.val() !== '') {
     matchButton.show();
     matchButton.attr('class', 'animated fadeIn delay-1s');
   } else {
-    matchButton.hide();
+    matchButton.attr('class', 'animated fadeOut');
+    matchButton.delay(1000).hide(0);
   }
+}
+
+input.keydown(function(){
+  clearTimeout(timer);
+  timer = setTimeout(doStuff, 1000, $(this))
 });
+
+function doStuff(inputThis) {
+  addCelebrityImage(inputThis);
+  displayMatchButton();
+}
