b0c0f7dd995f248dd5056dfccdb9ea931d97b432
Jibe0123 <Jbagostin@gmail.com>
2019-03-20 17:04:21 +0100

    remvove older dependancies && add new dependancies for php 7.3 compatibility 🔫

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-use Sunra\PhpSimple\HtmlDomParser;
+use KubAT\PhpSimple\HtmlDomParser;
 
 
 
