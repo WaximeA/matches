bbadc529b3dd83068da046b52023d59a948b1a02
waxime <aveline.maxime@gmail.com>
2019-03-21 22:53:15 +0100

    front: Manage global front

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -96,3 +96,19 @@ matchButton.on('click', function (e) {
     document.location.href = "http://localhost:8000/hot/match/"+matchLeftInput.val()+"/"+matchRightInput.val();
 });
 
+$('.tabgroup > div').hide();
+$('.tabgroup > div:first-of-type').show();
+$('.tabs a').click(function(e){
+  e.preventDefault();
+  var $this = $(this),
+      tabgroup = '#'+$this.parents('.tabs').data('tabgroup'),
+      others = $this.closest('li').siblings().children('a'),
+      target = $this.attr('href');
+  others.removeClass('active');
+  $this.addClass('active');
+  $(tabgroup).children('div').hide();
+  $(target).show();
+
+})
+
+
