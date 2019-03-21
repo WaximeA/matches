138e50c6dc3f5de281ec361dc5a7750c06523429
Jibe0123 <Jbagostin@gmail.com>
2019-03-21 14:36:47 +0100

    add feed with articles

--- src/Controller/MatchesController.php
+++ src/Controller/MatchesController.php
@@ -7,11 +7,18 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MatchesController extends AbstractController
 {
+
+
+
+    const API_TOKEN = "0f5ac6fc2aee41ba90a195c209dadef7";
+
     /**
      * @Route("/matches", name="matches")
      */
     public function index()
     {
+
+
         return $this->render('matches/index.html.twig', [
             'controller_name' => 'MatchesController',
         ]);
---
+++ src/Service/ArticleService.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Service;
+
+
+use GuzzleHttp\Client as Guzzle;
+use GuzzleHttp\Promise\Promise;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Stream;
+
+
+class ArticleService{
+
+
+    const APIKEY = "0f5ac6fc2aee41ba90a195c209dadef7";
+
+    public function getArticleIds($firstQuery, $secondQuery)
+    {
+        $url = 'https://api.ozae.com/gnw/uc/raw-search?q='. $firstQuery.'+'.$secondQuery.'&date=20180101__20190101&key='.self::APIKEY;
+
+
+        $client = new Guzzle();
+        $res = $client->request('GET', $url);
+
+        $body = $res->getBody();
+
+
+        //TODO : SI il n'y a pas de resultat
+        $ids = json_decode((string) $body)->articles_ids;
+
+        $output = array_slice($ids, 0, 10);
+
+        return $output;
+
+    }
+
+
+    public function getArticleById($id)
+    {
+        $url = 'https://api.ozae.com/gnw/article/'.$id.'?key='.self::APIKEY;
+
+        $client = new Guzzle();
+        $res = $client->request('GET', $url);
+
+        $body = $res->getBody();
+
+        $article = json_decode((string) $body);
+
+        return $article;
+    }
+
+}
